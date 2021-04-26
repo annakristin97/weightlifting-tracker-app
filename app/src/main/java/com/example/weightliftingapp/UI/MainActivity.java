2ca3b9608f1fff5c3785d7dac68dbbc5ca6501dc
Bergdis <65372344+Bergdis@users.noreply.github.com>
2021-04-26 19:08:57 +0200

    MultiSelectSpinner

--- app/src/main/java/com/example/weightliftingapp/UI/MainActivity.java
+++ app/src/main/java/com/example/weightliftingapp/UI/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.weightliftingapp.UI;
 
+
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,10 +17,20 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.androidbuts.multispinnerfilter.KeyPairBoolData;
+import com.androidbuts.multispinnerfilter.MultiSpinnerListener;
+import com.androidbuts.multispinnerfilter.MultiSpinnerSearch;
 import com.example.weightliftingapp.Entities.FilteredLifts;
 import com.example.weightliftingapp.Entities.Lift;
 import com.example.weightliftingapp.R;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.json.JSONArray;
@@ -28,6 +40,10 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import okhttp3.Call;
@@ -41,16 +57,60 @@ public class MainActivity extends AppCompatActivity {
     ListView liftList;
 
     Button btnBarChart, btnPieChart, searchButton;
+    public int[] colors = {Color.RED,Color.BLUE, Color.GREEN, Color.YELLOW, Color.BLACK, Color.GRAY, Color.WHITE, Color.CYAN, Color.MAGENTA, Color.DKGRAY};
+
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
     private FilteredLifts mFilteredLifts;
     private LiftAdapter mAdapter;
 
+    ArrayList<FilteredLifts> AllFilteredLifts = new ArrayList<>();
+
+    Button searchButton;
+    FloatingActionButton AddButton;
+    Spinner timeIntervalFilter, repsFilter, setsFilter;
+    Spinner typeFilter;
+    MultiSpinnerSearch multiSelectSpinnerWithSearch;
+    List<String> SelectedLifts = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final List<String> list = Arrays.asList(getResources().getStringArray(R.array.typeItems));
+
+        final List<KeyPairBoolData> listArray1 = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            KeyPairBoolData h = new KeyPairBoolData();
+            h.setId(i + 1);
+            h.setName(list.get(i));
+            h.setSelected(i < 1);
+            listArray1.add(h);
+        }
+
+        multiSelectSpinnerWithSearch = findViewById(R.id.multipleItemSelectionSpinner);
+
+        //multiSelectSpinnerWithSearch.setBackgroundColor(Color.GRAY);
+
+        // Pass true If you want searchView above the list. Otherwise false. default = true.
+        multiSelectSpinnerWithSearch.setSearchEnabled(true);
+
+        // A text that will display in search hint.
+        multiSelectSpinnerWithSearch.setSearchHint("Select lift");
+
+        // Set text that will display when search result not found...
+        multiSelectSpinnerWithSearch.setEmptyTitle("Not Data Found!");
+
+        // If you will set the limit, this button will not display automatically.
+        multiSelectSpinnerWithSearch.setShowSelectAllButton(true);
+
+        multiSelectSpinnerWithSearch.get
+
+        //A text that will display in clear text button
+        multiSelectSpinnerWithSearch.setClearText("Close & Clear");
+
 
 
         //kannski breyta her??
@@ -111,8 +171,47 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent I = new Intent(MainActivity.this, PieChartActivity.class);
                 startActivity(I);
+
+        // Removed second parameter, position. Its not required now..
+        // If you want to pass preselected items, you can do it while making listArray,
+        // pass true in setSelected of any item that you want to preselect
+
+        multiSelectSpinnerWithSearch.setItems(listArray1, new MultiSpinnerListener() {
+            @Override
+            public void onItemsSelected(List<KeyPairBoolData> items) {
+                //BREYTA
+                SelectedLifts.clear();
+                for (int i = 0; i < items.size(); i++) {
+                    if (items.get(i).isSelected()) {
+                        Log.i(TAG, i + " : " + items.get(i).getName() + " : " + items.get(i).isSelected());
+                        //store items that where selected
+                        SelectedLifts.add(items.get(i).getName());
+                    }
+                }
             }
         });
+
+        // click search button
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // update graph content
+                //AllFilteredLifts.clear();
+                getLifts(SelectedLifts,
+                        timeIntervalFilter.getSelectedItem().toString(),
+                        repsFilter.getSelectedItem().toString(),
+                        setsFilter.getSelectedItem().toString());
+            }
+
+        });
+
+    }
+
+
+
+    public void openAddFragment(){
+        Intent intent = new Intent(this, AddFragment.class);
+        startActivity(intent);
     }
 
     /**
@@ -124,26 +223,51 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         callBackend(request);
+    public void getLifts(List<String> liftNames, String timeInterval, String reps, String sets) {
+        List<Request> requests = new ArrayList<Request>();
+
+        for(int i = 0; i < liftNames.size(); i++){
+            Log.d(TAG, "liftName: " + i + "  is " + liftNames.get(i));
+            RequestBody formBody = new FormBody.Builder()
+                    .add("timeInterval", timeInterval)
+                    .add("liftName", liftNames.get(i))
+                    .add("sets", sets)
+                    .add("reps", reps)
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url("http://10.0.2.2:8090/lifts/search")
+                    .post(formBody)
+                    .build();
+            requests.add(request);
+        }
+        callBackend(requests);
     }
 
     /**
      * Kallar á bakenda til þess að sækja allar liftur
      */
-    private void callBackend(Request request){
+    private void callBackend(List<Request> requests){
+
         OkHttpClient client = new OkHttpClient();
 
         if(isNetworkAvailable()) {
-            Call call = client.newCall(request);
-            call.enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                        }
-                    });
-                    alertUserAboutError();
+            Log.d(TAG, "callBackend: requests size: " + requests.size());
+            for(int i = 0; i < requests.size(); i++ ) {
+                if (i == 0 ) {
+                    AllFilteredLifts.clear();
                 }
+                Call call = client.newCall(requests.get(i));
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                            }
+                        });
+                        alertUserAboutError();
+                    }
 
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
@@ -170,12 +294,62 @@ public class MainActivity extends AppCompatActivity {
                         Log.e(TAG, "Exception caught: ", e);
                     } catch (JSONException e) {
                         Log.e(TAG, "JSON caught: ", e);
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                            }
+                        });
+                        try {
+                            String jsonData = response.body().string();
+                            //Log.v(TAG, jsonData);
+                            if (response.isSuccessful()) {
+                                Log.d(TAG, "adding to AllfilteredLifts: " + parseLiftListDetails(jsonData));
+                                AllFilteredLifts.add(parseLiftListDetails(jsonData));
+
+                            } else {
+                                alertUserAboutError();
+                            }
+                        } catch (IOException | ParseException e) {
+                            Log.e(TAG, "Exception caught: ", e);
+                        } catch (JSONException e) {
+                            Log.e(TAG, "JSON caught: ", e);
+                        }
                     }
-                }
-            });
-        } else {
+                });
+            }
+
+        }else {
             Toast.makeText(this, "Network unavailable", Toast.LENGTH_LONG).show();
         }
+
+
+        //runOnUiThread(new Runnable() {
+         //   @Override
+          //  public void run() {
+           //     try {
+            //        Log.d(TAG, "updateDisplay() — updating item: " + AllFilteredLifts + "size of AllFilteredLifts: " +  AllFilteredLifts.size()) ;
+             //       updateDisplay();
+              //  } catch (ParseException e) {
+              //      e.printStackTrace();
+              //  }
+           // }
+        // });
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override public void run() {
+                try {
+                    Log.d(TAG, "updateDisplay() — updating item: " + AllFilteredLifts + "size of AllFilteredLifts: " +  AllFilteredLifts.size()) ;
+                    updateDisplay();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 2000);
+
+
     }
 
     /**
@@ -216,6 +390,83 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new LiftAdapter(this, mFilteredLifts.getLifts());
 
         liftList.setAdapter(mAdapter);
+    private void updateDisplay() throws ParseException {
+
+        // connect chart variable to the xml object
+        LineChart chart = findViewById(R.id.barchart);
+
+        // format the chart (colors, text size, label orientation,..)
+        chart.getXAxis().setValueFormatter(new MyXAxisValueFormatter());
+        chart.getXAxis().setTextColor(Color.WHITE);
+        chart.getXAxis().setTextSize(12);
+        chart.getXAxis().setLabelRotationAngle(45);
+        chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        chart.getAxisLeft().setTextColor(Color.WHITE);
+        chart.getAxisLeft().setTextSize(12);
+        chart.getAxisRight().setDrawLabels(false);
+        chart.getLegend().setTextColor(Color.WHITE);
+        chart.getLegend().setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        chart.getLegend().setTextSize(12);
+
+        if(AllFilteredLifts != null) {
+
+            //ADD color matrix
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+
+
+            for (int i = 0;i <AllFilteredLifts.size();i++) {
+
+                if (AllFilteredLifts.get(i) != null) {
+
+                    // retrieve current list of lifts
+                    List<Lift> list = AllFilteredLifts.get(i).getLifts();
+                    Log.d(TAG, "AllfilterLitfs.get(i) i = : " + i + "list = " + list);
+                    ArrayList liftList = new ArrayList();
+
+
+                    // sort by lift log time IMPORTANT
+                    Collections.sort(list, new Comparator<Lift>() {
+                        @Override
+                        public int compare(final Lift object1, final Lift object2) {
+                            return Long.compare(object1.getLogTime(), object2.getLogTime());
+                        }
+                    });
+
+                    for (int j = 0; j < list.size(); j++) {
+                        // create entry containing log time and weight of lift
+                        liftList.add(new BarEntry(list.get(j).getLogTime(), list.get(j).getWeight()));
+                    }
+
+                    ///HER!
+
+                    // create dataset
+                    LineDataSet bardataset = new LineDataSet(liftList, SelectedLifts.get(i)); //typeFilter.getSelectedItem().toString());
+                    Log.d(TAG, "selected lifts nr. " + i + " is " + SelectedLifts.get(i));
+
+                    // format graph line
+                    bardataset.setColor(colors[i]);
+                    bardataset.setCircleColor(Color.BLACK);
+                    bardataset.setFormSize(6);
+                    bardataset.setValueTextSize(0);
+
+                    dataSets.add(bardataset);
+
+
+                }
+                // animation on graph create
+                chart.animateY(2000);
+
+                // add data to chart
+                LineData data = new LineData(dataSets);
+                chart.setData(data);
+
+                // format line label text color
+                chart.getLineData().setValueTextColor(Color.WHITE);
+            }
+            
+        }
+
     }
 
     /**
@@ -236,5 +487,7 @@ public class MainActivity extends AppCompatActivity {
         AlertDialogFragment dialog = new AlertDialogFragment();
         dialog.show(getFragmentManager(), "error_dialog");
     }
+
+
 
 }
